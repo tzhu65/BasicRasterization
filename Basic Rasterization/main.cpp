ead063770e7364076921b050334bfa8801b53950
tzhu65 <tzhu65@gmail.com>
2016-03-18 10:47:14 -0400

    Initial Commit

---
+++ Basic Rasterization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Basic Rasterization
+//
+//  Created by Tony Zhu on 3/18/16.
+//  Copyright © 2016 Tony Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
